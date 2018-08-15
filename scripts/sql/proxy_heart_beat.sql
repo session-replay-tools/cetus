6b6396fb1d3bc191061596f591013a3a39bfc5cd
Bin Wang <wangbin579@gmail.com>
2018-08-15 10:36:30 +0800

    Update proxy_heart_beat.sql

--- scripts/sql/proxy_heart_beat.sql
+++ scripts/sql/proxy_heart_beat.sql
@@ -1,19 +1,3 @@
-/*
- Navicat Premium Data Transfer
-
- Source Server         : k8s-读写分离-主
- Source Server Type    : MariaDB
- Source Server Version : 100134
- Source Host           : 10.254.78.30:3306
- Source Schema         : proxy_heart_beat
-
- Target Server Type    : MariaDB
- Target Server Version : 100134
- File Encoding         : 65001
-
- Date: 18/07/2018 10:36:09
-*/
-
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 

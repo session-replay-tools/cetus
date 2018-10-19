f2889cf12ce62e343fa869a7a09d0fa20a01c7d4
lazio579 <wangbin5790@163.com>
2018-10-19 17:34:48 +0800

    Remove some logs

--- src/network-mysqld.c
+++ src/network-mysqld.c
@@ -4250,7 +4250,7 @@ network_mysqld_con_accept(int G_GNUC_UNUSED event_fd, short events, void *user_d
     network_mysqld_add_connection(listen_con->srv, client_con, FALSE);
 
     client_con->key = client_con->srv->sess_key++;
-    g_message("%s: accept a new client connection", G_STRLOC);
+    g_debug("%s: accept a new client connection", G_STRLOC);
 
     /**
      * inherit the config to the new connection 

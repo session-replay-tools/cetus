8f2cf483c100823686417b88a17c2d41dbfd8e27
lazio579 <wangbin5790@163.com>
2018-11-19 16:25:09 +0800

    create new connections when removing old server connections

--- src/network-mysqld.c
+++ src/network-mysqld.c
@@ -5175,6 +5175,7 @@ check_and_create_conns_func(int fd, short what, void *arg)
     if (chas->need_to_refresh_server_connections) {
         close_old_server_connetions(chas);
         chas->need_to_refresh_server_connections = 0;
+        chas->is_need_to_create_conns = 1;
     }
 
     if (!chas->maintain_close_mode) {

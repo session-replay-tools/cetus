a739c7f443eee97e2d4e7c0a4b082645bdffbd75
lazio579 <wangbin5790@163.com>
2018-12-03 08:28:52 +0800

    Remove one memory leak

--- src/chassis-config.c
+++ src/chassis-config.c
@@ -451,10 +451,13 @@ chassis_config_local_query_object(chassis_config_t *conf,
         }
         g_clear_error(&err);
         g_free(object_file);
+        if (buffer) {
+            g_free(buffer);
+        }
         return FALSE;
     }
 
-    *json_res = g_strdup(buffer);
+    *json_res = buffer;
 
     GStatBuf sta;
     if (g_stat(object_file, &sta) == 0) {
